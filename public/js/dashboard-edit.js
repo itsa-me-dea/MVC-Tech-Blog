606ab31fbb7e45e5eff9571d0bd4eb91f3f4cb05
itsa-me-dea <wachadea@gmail.com>
2023-09-05 12:46:20 -0700

    fixed update button js

---
+++ public/js/dashboard-edit.js
@@ -0,0 +1,26 @@
+document.addEventListener('submit', async (event) => {
+  if (event.target.classList.contains('edit-post-form')) {
+    event.preventDefault();
+
+    const id = event.target.querySelector('.btn-update').getAttribute('data-id');
+    console.log("ID:", id);
+    const name = event.target.querySelector('#post-name').value.trim();
+    const description = event.target.querySelector('#post-desc').value.trim();
+
+    if (name && description) {
+      const response = await fetch(`/api/posts/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({ name, description }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.ok) {
+        document.location.replace('/dashboard/');
+      } else {
+        alert('Failed to update post');
+      }
+    }
+  }
+});
